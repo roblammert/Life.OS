0b23809f7b23188c00f6756fbe704a7e4f653fed
Rob Lammert <rob.lammert@gmail.com>
2026-03-02 17:47:40 -0600

    Post-Merge Fixes, part deux

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
 
-const container = document.getElementById('root')
-if (!container) {
-	throw new Error('Root container missing in HTML')
+const root = document.getElementById("root");
+if (!root) throw new Error("Missing #root element");
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+// Re-enable SW (register in production, clean up in development)
+if ("serviceWorker" in navigator) {
+  if (import.meta.env.PROD) {
+    window.addEventListener("load", () => {
+      void navigator.serviceWorker.register("/service-worker.js");
+    });
+  } else {
+    void navigator.serviceWorker.getRegistrations().then((regs) => {
+      regs.forEach((r) => void r.unregister());
+    });
+    if ("caches" in window) {
+      void caches.keys().then((keys) => keys.forEach((k) => void caches.delete(k)));
+    }
+  }
 }
-
-createRoot(container).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-)
